57119a203f0bdc64ae0ac186b1fb0639e21f7feb
Choirul Nurrohim <44932941+cnurrohim@users.noreply.github.com>
2022-06-12 16:04:48 +0700

    try to fix CORS error

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,6 @@ import axios from "axios"
 const API = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
   headers: {
-    "Content-Type": "application/json",
     "x-api-key": process.env.REACT_APP_API_KEY,
   },
 })
